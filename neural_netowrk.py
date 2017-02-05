6ade2a6ecf12f91cb9b7a8107f68eef6b41668ba
arp1561 <joshiarpit2@gmail.com>
2017-02-05 11:57:54 +0530

    Added comments

--- neural_netowrk.py
+++ neural_netowrk.py
@@ -3,16 +3,21 @@ import sys
 import os
 
 class NeuralNetwork:
+	''' Initialization of weights '''
 	def __init__(self):
 		self.synaptic_weights =np.random.random((3,1))
 		print "Initial value of weights = \n"+str(self.synaptic_weights)
 
+	'''Returns sigmoidvalue'''
 	def sigmoid(self,x):
 		return 1/(1+np.exp(-x))
 	
+	'''Returns value of sigmoid derivative'''
 	def sigmoidDerivative(self,x):
 		return x*(1-x)
 	
+
+	'''Trains the neural network and adjusts the weights'''
 	def train(self,training_set_inputs,training_set_outputs,iterations):
 		for iteration in range(iterations):
 				print "iterations = "+str(iteration)
@@ -25,11 +30,13 @@ class NeuralNetwork:
 				self.synaptic_weights+=adjustment
 				print "Updated Weights = \n" + str(self.synaptic_weights)
 				os.system('clear')
-	
+
+	'''Output Function'''
 	def think(self,inputs):
 		return self.sigmoid(np.dot(inputs,self.synaptic_weights))
-
+'''main function'''
 def main(argv):
+	
 	neural_network = NeuralNetwork()
 	training_sets_input = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
 	training_sets_output = np.array([[0,1,1,0]]).T
